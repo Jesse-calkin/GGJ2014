61fac8b5c217a240d14de67416611425631e1590
jesse-calkin <jesse.calkin@gmail.com>
2014-01-14 09:10:07 -0700

    added basic player stuff, constants, spritesheet functions

--- classes/platformer/game.py
+++ classes/platformer/game.py
@@ -1,26 +1,14 @@
 import pygame
 from random import choice
+from constants import *
+from player import *
 
 """Base Game
 
 This is a basic game stub from which to expand upon.
 """
 
-#Global Game Constants
-SCREEN_WIDTH = 1440
-SCREEN_HEIGHT = 600
-FRAME_RATE = 30
-DELTA_TIME = pygame.time.Clock().tick(60)
 
-#Colors!
-RED = (255,000,000)
-ORANGE = (255,165,000)
-YELLOW = (255,255,000)
-GREEN = (000,255,000)
-BLUE = (000,000,255)
-INDIGO = (75,000,130)
-VIOLET = (143,000,255)
-TASTE_THE_RAINBOW = [RED,ORANGE,YELLOW,GREEN,BLUE,INDIGO,VIOLET]
 
 #Input
 LEFT_KEY = pygame.K_LEFT
@@ -33,10 +21,17 @@ ESC_KEY = pygame.K_ESCAPE
 
 class Game(object):
     def main(self, screen):
-    	clock = pygame.time.Clock()
-
+    	""" game stuff """
+        clock = pygame.time.Clock()
     	running = True
-    	
+
+        """ sprite stuff """
+        player = Player()
+        player.rect.y = 400
+        player.rect.x = 400
+    	sprite_group = pygame.sprite.Group()
+        sprite_group.add(player)
+
     	""" hey look! A Game loop! """
         while running:
             for event in pygame.event.get():
@@ -46,7 +41,9 @@ class Game(object):
                     running = False
             bgcolor = choice(TASTE_THE_RAINBOW)
             screen.fill(bgcolor)
+            sprite_group.draw(screen)
             pygame.display.flip()
+            clock.tick(60)
 
 if __name__ == '__main__':
     pygame.init()
---
+++ classes/platformer/player.py
@@ -0,0 +1,24 @@
+import pygame
+from spritesheet_functions import *
+
+"""
+Player class
+"""
+
+
+class Player(pygame.sprite.Sprite):
+
+    # Constructor. Can also take color, width, height
+    def __init__(self):
+
+       # Call the parent class (Sprite) constructor - like calling super
+       pygame.sprite.Sprite.__init__(self)
+
+       # Create an image of the block, and fill it with a color.
+       # This could also be an image loaded from the disk.
+       sprite_sheet = SpriteSheet("../../resources/sprites/nyan_cat.png")
+       self.image = sprite_sheet.getImage(13, 131, 32, 30)
+
+       # Fetch the rectangle object that has the dimensions of the image
+       # Update the position of this object by setting the values of rect.x and rect.y
+       self.rect = self.image.get_rect()
