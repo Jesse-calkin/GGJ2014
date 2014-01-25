b28b911c59995348bd09e899dace7780f0fb0cf0
Chris Weathers <chris@doubleencore.com>
2014-01-25 14:30:55 -0700

    Converted sound code to object/class methods.

--- classes/platformer/sound.py
+++ classes/platformer/sound.py
@@ -1,71 +1,69 @@
 import pygame
 
+class Sound(object):
 
-"Public"
+	# 'sounds' - a list of tuples of the format (sound ID, sound filename)
+	# Result - 'loaded_sounds' is populated with sound data for the given 'sounds'
+	@classmethod
+	def load_sounds(cls, sounds):
+		cls.loaded_sounds.clear()
 
-# 'sounds' - a list of tuples of the format (sound ID, sound filename)
-# Result - 'loaded_sounds' is populated with sound data for the given 'sounds'
-def load_sounds(sounds):
-	loaded_sounds.clear()
+		for sound_tuple_to_load in sounds:
+			sound_id = sound_tuple_to_load[cls.sound_tuple_index_sound_id]
+			sound_filename = sound_tuple_to_load[cls.sound_tuple_index_sound_filename]
+			sound = pygame.mixer.Sound(sound_filename)
 
-	for sound_tuple_to_load in sounds:
-		sound_id = sound_tuple_to_load[sound_tuple_index_sound_id]
-		sound_filename = sound_tuple_to_load[sound_tuple_index_sound_filename]
-		sound = pygame.mixer.Sound(sound_filename)
+			cls.loaded_sounds[sound_id] = (sound_id, sound_filename, sound)
 
-		loaded_sounds[sound_id] = (sound_id, sound_filename, sound)
+	@classmethod
+	def play_sound_for_sound_id(cls, sound_id):
+		sound = cls.sound_for_sound_id(sound_id)
+		sound.play()
 
-def play_sound_for_sound_id(sound_id):
-	sound = sound_for_sound_id(sound_id)
-	sound.play()
+	@classmethod
+	def stop_sound_for_sound_id(sound_id):
+		sound = cls.sound_for_sound_id(sound_id)
+		sound.stop()
 
-def stop_sound_for_sound_id(sound_id):
-	sound = sound_for_sound_id(sound_id)
-	sound.stop()
+	# keys - sound IDs
+	# values - sound tuples of the format (sound ID, sound filename, Sound object)
+	loaded_sounds = {}
 
+	# sound tuple value indices
+	sound_tuple_index_sound_id       = 0
+	sound_tuple_index_sound_filename = 1
+	sound_tuple_index_sound          = 2
 
-"Private"
+	@classmethod
+	def start(cls):
+		pygame.mixer.init()
 
-# keys - sound IDs
-# values - sound tuples of the format (sound ID, sound filename, Sound object)
-loaded_sounds = {}
+	@classmethod
+	def stop(cls):
+		pygame.mixer.quit()
 
-# sound tuple value indices
-sound_tuple_index_sound_id       = 0
-sound_tuple_index_sound_filename = 1
-sound_tuple_index_sound          = 2
+	@classmethod
+	def sound_tuple_for_sound_id(cls, sound_id):
+		sound_tuple = cls.loaded_sounds[sound_id]
+		return sound_tuple
 
-def start():
-	pygame.mixer.init()
+	@classmethod
+	def sound_for_sound_id(cls, sound_id):
+		sound_tuple = cls.sound_tuple_for_sound_id(sound_id)
+		sound = sound_tuple[cls.sound_tuple_index_sound]
+		return sound
 
-def stop():
-	pygame.mixer.quit()
-
-def sound_tuple_for_sound_id(sound_id):
-	sound_tuple = loaded_sounds[sound_id]
-	return sound_tuple
-
-def sound_for_sound_id(sound_id):
-	sound_tuple = sound_tuple_for_sound_id(sound_id)
-	sound = sound_tuple[sound_tuple_index_sound]
-	return sound
-
-
-"Test"
-
-def test_sounds():
-	sound_id = "test"
-	sound_filename = "../../resources/sounds/test.ogg"
-	sounds = [(sound_id, sound_filename)]
-	load_sounds(sounds)
-	play_sound_for_sound_id(sound_id)
-	for x in xrange(1,50000000):
-		pass
-
-
-"Main"
+	@classmethod
+	def test_sounds(cls):
+		sound_id = "test"
+		sound_filename = "../../resources/sounds/test.ogg"
+		sounds = [(sound_id, sound_filename)]
+		cls.load_sounds(sounds)
+		cls.play_sound_for_sound_id(sound_id)
+		for x in xrange(1,50000000):
+			pass
 
 if __name__ == '__main__':
-	start()
-	test_sounds()
-	stop()
+	Sound.start()
+	Sound.test_sounds()
+	Sound.stop()
