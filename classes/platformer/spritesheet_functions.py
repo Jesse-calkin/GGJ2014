375f7763d9d55cd4f66fa2683f7008123142d8b9
BSharp985 <davis.brandon.a@gmail.com>
2014-01-24 23:54:20 -0700

    readd missing close paren to spritesheet

--- classes/platformer/spritesheet_functions.py
+++ classes/platformer/spritesheet_functions.py
@@ -27,7 +27,7 @@ class SpriteSheet():
         image = pygame.Surface([width, height]).convert()
 
         # Copy the sprite from the large sheet onto the smaller image
-        image.blit(self.sprite_sheet, (0, 0), (x, y, width, height)
+        image.blit(self.sprite_sheet, (0, 0), (x, y, width, height))
 
         # Assuming black works as the transparent color
         image.set_colorkey(BLACK)
