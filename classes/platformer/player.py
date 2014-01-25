28fa05de7b7449f3fbe8beca619b13afe29a04a2
jesse-calkin <jesse.calkin@gmail.com>
2014-01-25 01:26:07 -0700

    changed width in constants and started on player movement

--- classes/platformer/player.py
+++ classes/platformer/player.py
@@ -1,5 +1,6 @@
 import pygame
 from spritesheet_functions import *
+import pygame.math
 
 """
 Player class
@@ -7,18 +8,23 @@ Player class
 
 
 class Player(pygame.sprite.Sprite):
-
+    """ 
+    Initialize and set default vectors
+    """
+    position = pygame.math.Vector2(0,0)
+    impulse = pygame.math.Vector2(0,0)
     # Constructor. Can also take color, width, height
     def __init__(self):
 
         # Call the parent class (Sprite) constructor - like calling super
         pygame.sprite.Sprite.__init__(self)
 
-        # Create an image of the block, and fill it with a color.
-        # This could also be an image loaded from the disk.
-        sprite_sheet = SpriteSheet("../../resources/sprites/nyan_cat.png")
+        sprite_sheet = SpriteSheet("../../resources/sprites/pac.png")
         self.image = sprite_sheet.getImage(13, 131, 32, 30)
 
         # Fetch the rectangle object that has the dimensions of the image
         # Update the position of this object by setting the values of rect.x and rect.y
         self.rect = self.image.get_rect()
+    def applyImpulse(vec2):
+        impulseToApply = vec2
+        pass
