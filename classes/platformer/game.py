39a8075040bd27e0f865fd63bd444a317adac254
Chris Weathers <chris@doubleencore.com>
2014-01-25 20:33:50 -0700

    Changed scoring to per-level.

--- classes/platformer/game.py
+++ classes/platformer/game.py
@@ -31,7 +31,7 @@ JUMP_KEY = pygame.K_a
 FULLSCREEN_KEY = pygame.K_f
 
 class Game(object):
-    branch_scores =[0,0]
+    level_score = 0
     total_score = 0
     evolv_threshold = 10
     is_fullscreen = False
@@ -46,18 +46,10 @@ class Game(object):
         pass
 
     def should_transition(self):
-        if self.branch_scores[0] >= self.level.powerups_goal:
-            return True,1
-        elif self.branch_scores[1] >= self.level.powerups_goal:
-            return True,2
-
-    def update_scores(self, score_type):
-        # pass in a powerup.score_type
-        if score_type==1:
-            self.branch_scores[0]+=1
-        if score_type==2:
-            self.branch_scores[1]+=1
-        self.total_score += 1
+        if self.level_score >= self.level.target_score:
+            return True
+        else:
+            return False
 
     def toggle_fullscreen(self):
         self.paused = True
@@ -148,10 +140,8 @@ class Game(object):
                         #     player.jump()
             if is_moving_up:
                 player.move_up()
-                self.update_scores(2)
             elif is_moving_down:
                 player.move_down()
-                self.update_scores(1)
 
             #If we aren't paused, do this stuff
             if not paused:
@@ -166,7 +156,8 @@ class Game(object):
             if powerup:
                 print "collided with powerup: %s" % powerup.powerup_type
                 powerup.collided(player)
-                self.update_scores(powerup.powerup_type)
+                self.level_score = self.level_score + 1
+                self.total_score = self.total_score + 1
 
             block = pygame.sprite.spritecollideany(player, block_mgr.obstacle_group)
             if block:
