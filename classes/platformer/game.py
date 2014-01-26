b48e2e7a6e3b013041162963d24129389fbc41fe
jesse-calkin <jesse.calkin@gmail.com>
2014-01-25 23:10:02 -0700

    player gets gravity from the level now

--- classes/platformer/game.py
+++ classes/platformer/game.py
@@ -33,6 +33,7 @@ FULLSCREEN_KEY = pygame.K_f
 class Game(object):
     level_score = 0
     total_score = 0
+    player = None
     evolv_threshold = 10
     is_fullscreen = False
     world_speed = 1.0  # 1x
@@ -48,8 +49,8 @@ class Game(object):
         Game.bg.add(self.level.background_filepath, 5)
         Game.bg.add(self.level.foreground_filepath, 2)
 
-    def update_player_for_current_level(self):
-        pass
+    def update_player_for_current_level(self, player):
+        player.level = self.level
 
     def should_transition(self):
         if self.level_score >= self.level.target_score:
@@ -72,7 +73,7 @@ class Game(object):
 
     def update_for_current_level(self):
         self.update_background_images_for_current_level()
-        self.update_player_for_current_level()
+        self.update_player_for_current_level(self.player)
 
     def toggle_fullscreen(self):
         self.paused = True
@@ -93,9 +94,9 @@ class Game(object):
         paused = False
 
         """ sprite stuff """
-        player = Player()
+        self.player = Player()
         player_group = pygame.sprite.Group()
-        player_group.add(player)
+        player_group.add(self.player)
 
         enemy = Enemy()
         enemy_group = pygame.sprite.Group()
@@ -165,35 +166,35 @@ class Game(object):
                         # if event.key == JUMP_KEY and player.on_ground:
                         #     player.jump()
             if is_moving_up:
-                player.move_up()
+                self.player.move_up()
             elif is_moving_down:
-                player.move_down()
+                self.player.move_down()
 
             #If we aren't paused, do this stuff
             if not paused:
                 # Update operaions
                 Game.bg.scroll(speed*self.world_speed)
-                player.update(delta_time)
+                self.player.update(delta_time)
                 enemy_group.update(delta_time)
                 block_mgr.update(self.world_speed, delta_time)
                 powerup_mgr.update(self.world_speed, delta_time)
 
-            powerup = pygame.sprite.spritecollideany(player, powerup_mgr.group)
-            if powerup:
-                print "collided with powerup: %s" % powerup.powerup_type
-                Sound.play_sound_for_sound_id(sound_id_eat)
-                powerup.collided(player)
-                self.level_score = self.level_score + 1
-                self.total_score = self.total_score + 1
-                if self.should_transition():
-                    self.transition_to_next_level()
+                powerup = pygame.sprite.spritecollideany(self.player, powerup_mgr.group)
+                if powerup:
+                    print "collided with powerup: %s" % powerup.powerup_type
+                    Sound.play_sound_for_sound_id(sound_id_eat)
+                    powerup.collided(self.player)
+                    self.level_score = self.level_score + 1
+                    self.total_score = self.total_score + 1
+                    if self.should_transition():
+                        self.transition_to_next_level()
 
-            block = pygame.sprite.spritecollideany(player, block_mgr.obstacle_group)
-            if block:
-                player.rect.bottom = block.rect.top
-                player.on_ground = True
-                print 'on ground'
-                player.impulse.y = 0
+                block = pygame.sprite.spritecollideany(self.player, block_mgr.obstacle_group)
+                if block:
+                    self.player.rect.bottom = block.rect.top
+                    self.player.on_ground = True
+                    print 'on ground'
+                    self.player.impulse.y = 0
 
 
             # Drawing operations
